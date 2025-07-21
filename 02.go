643e00dd4773fdda8f43fac1347c38b44b723a68
Eleanor McHugh <eleanor@games-with-brains.com>
2025-07-21 17:19:08 +0100

    HTTPS Hello World

---
+++ 02.go
@@ -0,0 +1,23 @@
+package main
+import (
+	"fmt"
+	"net/http"
+)
+
+const MESSAGE = "hello world"
+const ADDRESS = ":1024"
+
+const CERT_FILE = "02_cert.pem"
+const KEY_FILE = "02_key.pem"
+
+func main() {
+	http.HandleFunc("/hello", Hello)
+	if e := http.ListenAndServeTLS(ADDRESS, CERT_FILE, KEY_FILE, nil); e != nil {
+		fmt.Println(e)
+	}
+}
+
+func Hello(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain")
+	fmt.Fprintf(w, MESSAGE)
+}
