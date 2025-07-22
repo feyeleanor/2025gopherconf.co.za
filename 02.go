07b17c6dbe412e343a458f5bcc9b66600f2a5723
Eleanor McHugh <eleanor@games-with-brains.com>
2025-07-22 13:03:03 +0100

    inlined Hello() func to get readers accustomed to higher order functional style

--- 02.go
+++ 02.go
@@ -11,13 +11,12 @@ const CERT_FILE = "02_cert.pem"
 const KEY_FILE = "02_key.pem"
 
 func main() {
-	http.HandleFunc("/hello", Hello)
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain")
+		fmt.Fprintf(w, MESSAGE)
+	})
+
 	if e := http.ListenAndServeTLS(ADDRESS, CERT_FILE, KEY_FILE, nil); e != nil {
 		fmt.Println(e)
 	}
-}
-
-func Hello(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain")
-	fmt.Fprintf(w, MESSAGE)
 }
